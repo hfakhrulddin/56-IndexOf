0c7ff66bdfbba8a814893021b98b68b0d4fc66f2
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-05 23:17:16 -0700

    init3

---
+++ Indexof-Key/Indexof-Key/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexof_Key
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Print("collect");
+                Console.ReadLine();
+        }
+
+        public static void Print(string s)
+        {
+
+            foreach (char c in s)
+            {
+                if (s.IndexOf(c) == 2)
+                {
+                    Console.WriteLine("*");
+                }
+                else {
+                    Console.WriteLine(c);
+                }
+            }
+        }
+    }
+}
